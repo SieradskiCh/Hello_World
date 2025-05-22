0957adf819f99fc3936385d60d223b3518d87cfd
SieradskiCh <cmsieradski@gmail.com>
2025-05-22 16:18:55 -0500

    Removed print statement, added exclamation

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,3 @@
-name = input("What's your name? \n")
-print('Hello ' + name)
-print('I AM STEVE!')
+name = input("What's your name? \n") #input from command line
+print('Hello', name + '!')
+#print('I AM STEVE!')
