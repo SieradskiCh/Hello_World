24bde37876bd691724b406c543c82063a33f42a5
SieradskiCh <cmsieradski@gmail.com>
2025-05-22 17:19:26 -0500

    Final commit - hello world complete

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,8 +5,11 @@ nameList = name.split(' ')
 while '' in nameList:
     nameList.remove('')
 
-print(nameList)
 
 #print('Hello', name + '!')
-print(f'Hello {nameList[0]} {nameList[-1]}!') #F string to embed variables
+if len(nameList) == 1:
+    print(f'Hello {nameList[0]}!')
+
+else:
+    print(f'Hello {nameList[0]} {nameList[-1]}!') #F string to embed variables
 #print('I AM STEVE!')
