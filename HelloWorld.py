0a741a3463a13aabbb147dde985b66e9c34da0c3
SieradskiCh <cmsieradski@gmail.com>
2025-05-22 16:53:44 -0500

    Code optimization

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,4 @@
-name = input("What's your name? \n") #input from command line
+name = input("What's your name? \n").title().strip() #input from command line
 print('Hello', name + '!')
-print(f'Hello {name.capitalize().strip()}!') #F string to embed variables
+print(f'Hello {name}!') #F string to embed variables
 #print('I AM STEVE!')
